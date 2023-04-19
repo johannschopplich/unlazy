43b71ed7a06960d3f3f12ae34512a538101a296c
Johann Schopplich <mail@johannschopplich.com>
2023-04-19 22:08:44 +0200

    chore: use vue-tsc for linting

--- packages/vue/build.config.ts
+++ packages/vue/build.config.ts
@@ -3,7 +3,6 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: [
     { builder: 'mkdist', input: './src/' },
-    { builder: 'mkdist', input: './src/', format: 'cjs', ext: 'js' },
   ],
   declaration: true,
   clean: true,
