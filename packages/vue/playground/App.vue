0e65a851251ae16fb9d6ef979308d879bb8a942a
Johann Schopplich <mail@johannschopplich.com>
2023-04-20 15:00:47 +0200

    chore: add `@unlazy/vue` playground

---
+++ packages/vue/playground/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { LazyImage } from '../src/components'
+</script>
+
+<template>
+  <LazyImage
+    blurhash="LKO2:N%2Tw=w]~RBVZRi};RPxuwH"
+    auto-sizes
+    data-srcset="image-320w.jpg 320w, image-640w.jpg 640w"
+    width="640"
+    height="640"
+  />
+</template>
