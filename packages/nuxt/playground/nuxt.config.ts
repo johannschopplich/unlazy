0860322fe51f314f36cb044350a1526af4ae753b
Johann Schopplich <mail@johannschopplich.com>
2023-04-23 12:55:05 +0200

    chore(nuxt): resolve unlazy to src instead of dist

--- packages/nuxt/playground/nuxt.config.ts
+++ packages/nuxt/playground/nuxt.config.ts
@@ -1,5 +1,25 @@
+import {resolve} from 'node:path'
+
+const currentDir = new URL('.', import.meta.url).pathname
+
 export default defineNuxtConfig({
   modules: ['@unocss/nuxt', '../src/module'],
+
+  vite: {
+    resolve: {
+      alias: {
+        '@unlazy/core': `${resolve(currentDir, '../../core/src')}/`,
+        'unlazy': `${resolve(currentDir, '../../unlazy/src')}/`,
+      },
+    },
+
+    optimizeDeps: {
+      exclude: [
+        '@unlazy/core',
+        'unlazy',
+      ],
+    },
+  },
 
   typescript: {
     shim: false,
