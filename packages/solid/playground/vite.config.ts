42d4a6e045799cbfe931e2991e9a56dd2113ab8b
Johann Schopplich <mail@johannschopplich.com>
2023-11-23 09:43:29 +0100

    chore: fix pending Vite 5 Solid plugin support

--- packages/solid/playground/vite.config.ts
+++ packages/solid/playground/vite.config.ts
@@ -5,6 +5,7 @@ import solid from 'vite-plugin-solid'
 const currentDir = new URL('.', import.meta.url).pathname
 
 export default defineConfig({
+  // @ts-expect-error: Vite 5 support pending
   plugins: [solid()],
 
   // Custom alias for unlazy until Jiti issue is resolved
