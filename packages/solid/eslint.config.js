9cd11bd9908579516dfb5c0026b5fb48b3ff5cc4
Johann Schopplich <mail@johannschopplich.com>
2024-04-19 10:35:04 +0200

    chore: revert solid linting changes

--- packages/solid/eslint.config.js
+++ packages/solid/eslint.config.js
@@ -1,8 +1,21 @@
+import solidPlugin from 'eslint-plugin-solid'
+import * as tsParser from '@typescript-eslint/parser'
 import antfu from '@antfu/eslint-config'
 
 export default await antfu(
   {
     vue: false,
-    solid: true,
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      solid: solidPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      ...solidPlugin.configs.recommended.rules,
+    },
   },
 )
