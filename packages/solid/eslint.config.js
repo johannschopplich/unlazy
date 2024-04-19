106cbdfd1e32092309e2656fcfc890994709f870
Johann Schopplich <mail@johannschopplich.com>
2024-04-19 10:38:09 +0200

    chore: downgrade ESLint to v8

--- packages/solid/eslint.config.js
+++ packages/solid/eslint.config.js
@@ -1,21 +1,6 @@
-import solidPlugin from 'eslint-plugin-solid'
-import * as tsParser from '@typescript-eslint/parser'
 import antfu from '@antfu/eslint-config'
 
-export default await antfu(
-  {
-    vue: false,
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    plugins: {
-      solid: solidPlugin,
-    },
-    languageOptions: {
-      parser: tsParser,
-    },
-    rules: {
-      ...solidPlugin.configs.recommended.rules,
-    },
-  },
-)
+export default await antfu({
+  vue: false,
+  solid: true,
+})
