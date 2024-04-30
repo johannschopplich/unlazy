224e96501f565a1caa46d52505ed6e95488c10c9
Johann Schopplich <mail@johannschopplich.com>
2024-04-30 17:56:40 +0200

    chore: resolve current path with `fileURLToPath` related #48

--- packages/unlazy/vite.config.bundle.ts
+++ packages/unlazy/vite.config.bundle.ts
@@ -1,7 +1,8 @@
 import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
-const currentDir = new URL('.', import.meta.url).pathname
+const currentDir = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
   build: {
