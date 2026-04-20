f9e2025f8d24a5b4c514a0c15cdd171f63d0feb0
Johann Schopplich <johann@schopplich.com>
2026-04-20 10:00:48 +0200

    chore: update file patterns in ESLint config for React and Solid

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,8 +4,8 @@ export default antfu({
   svelte: true,
   ignores: ['**/.svelte-kit/**'],
 })
-  .append(react({ files: ['packages/react/**/*.?([cm])[jt]s?(x)'] }))
-  .append(solid({ files: ['packages/solid/**/*.?([cm])[jt]s?(x)'] }))
+  .append(react({ files: ['packages/react/**/*.{ts,tsx}'] }))
+  .append(solid({ files: ['packages/solid/**/*.{ts,tsx}'] }))
   .append({
     files: ['packages/*/playground/**', 'packages/svelte/src/routes/**'],
     rules: {
