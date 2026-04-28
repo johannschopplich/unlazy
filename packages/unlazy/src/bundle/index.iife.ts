66c661e246692d3bc29c27142f8633cd9023ec15
Johann Schopplich <johann@schopplich.com>
2026-04-28 14:19:20 +0200

    fix: remove stale legacy default export

--- packages/unlazy/src/bundle/index.iife.ts
+++ packages/unlazy/src/bundle/index.iife.ts
@@ -1,13 +1,6 @@
-import { autoSizes, lazyLoad, triggerLoad } from '../../../core/src'
+import { lazyLoad } from '../../../core/src'
 
 export { autoSizes, lazyLoad, triggerLoad } from '../../../core/src'
-
-// Default export for IIFE bundle
-export default Object.freeze({
-  autoSizes,
-  lazyLoad,
-  triggerLoad,
-})
 
 // Automatically initiate if `init` attribute is present
 if (document.currentScript?.hasAttribute('init'))
